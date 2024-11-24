a4db3d3531ec87e8c9474bd4460bcccf8587c152
SEGR02 <sebastian.gomz02@gmail.com>
2024-11-23 23:54:07 -0500

    Fix from Sebas: change to post to receive body

--- src/app.ts
+++ src/app.ts
@@ -11,7 +11,7 @@ app.use(cors());
 app.use(morgan("tiny"));
 app.use(express.json({ limit: "10mb" }));
 
-app.get("/", (req: any, res: any) => {
+app.post("/", (req: any, res: any) => {
   try {
     const { email } = req.body;
     const nodemailer = require("nodemailer");
